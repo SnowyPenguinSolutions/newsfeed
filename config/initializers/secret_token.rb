d4afade11b213bab4beb3731ad63c6d69bec3d67
James Gifford & Elijah Snow-Rackley (Pairing) <pairing@jamesrgifford.com>
2013-02-18 13:31:35 -0500

    Initial commit of rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsfeed::Application.config.secret_token = '56411b116fe2ebdfd682d35b2f495d696a3061b86d0e874a9f03b6cb5cbfd68cc23ab6ee9b997e7c87ea8570eefd903b9ffef01d03ab32c781b4d88190ec30fe'
